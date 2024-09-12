8b2ef6ce3c127b1c2e162396c4495bbc1ad5d039
siraj chaudhary <mr.sirajchaudhary@yahoo.com>
2024-09-13 05:15:38 +0530

    first commit

---
+++ src/main/java/com/sirajchaudhary/springboot/jkube/example/SpringbootJkubeExampleApplication.java
@@ -0,0 +1,13 @@
+package com.sirajchaudhary.springboot.jkube.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJkubeExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJkubeExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sirajchaudhary/springboot/jkube/example/SpringbootJkubeExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sirajchaudhary.springboot.jkube.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJkubeExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
